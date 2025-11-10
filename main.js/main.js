821aa2882a9b828003729784f491f549cd8d5fb1
Sohila Hussein <sohilaahusseinn@gmail.com>
2025-11-10 19:17:23 +0200

    initial commit

---
+++ main.js/main.js
@@ -0,0 +1,8 @@
+let typed = new Typed('.auto-type', {
+    strings: ['Designer', 'Developer','lary Daniel'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  })
